6807ed9ea359782c2d948f4d9fb86a83e46d44b9
Jackie Ha <jha2@wellesley.edu>
2018-08-26 17:42:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,35 +1,18 @@
-def reverse_each_word(sentence)
-  string = sentence.split("")
-  reversed_sentence = []
-  string.each do |word|
-    reversed_sentence << word.reverse
+def reverse_each_word(string)
+  array = string.split
+  reverse_array = []
+  array.each do |word|
+    reverse_word = word.reverse
+    reverse_array.push(reverse_word)
   end 
-  return reversed_sentence.join(" ")
-end
+  return reverse_array.join(" ")
+end 
 
-def reverse_each_word(sentence)
-  string = sentence.split("").collect do |word|
-    word.
-  string.collect do |word|
-    reversed_sentence << word.reverse
+def reverse_each_word(string)
+  array = string.split
+  reverse_array = [] 
+  array.collect do |word|
+    reverse_array.push(word.reverse)
   end
-  return reversed_sentence.join(" ")
-end
-
-
-#def reverse_each_word(sentence)
- # new_array = sentence.split(" ")
-  #new_array.collect do |word|
-   # word.reverse
- # end 
-  #new_array.join(" ")
-# end 
-
-# def reverse_each_word(sentence)
-  # new_array = sentence.split(" ")
-  # reverse_array = []
-  # new_array.each do |word|
-    #reverse_array << word.reverse
-  #end 
-  #return new_array.join(" ")
-#end
+  reverse_array.join(" ")
+end 
