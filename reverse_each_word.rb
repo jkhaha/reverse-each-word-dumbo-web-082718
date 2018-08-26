6dddb5aa504af14d83209a3777eabfd1637ac637
Jackie Ha <jha2@wellesley.edu>
2018-08-26 17:37:24 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,35 @@
+def reverse_each_word(sentence)
+  string = sentence.split("")
+  reversed_sentence = []
+  string.each do |word|
+    reversed_sentence << word.reverse
+  end 
+  return reversed_sentence.join(" ")
+end
+
+def reverse_each_word(sentence)
+  string = sentence.split("")
+  reversed_sentence = []
+  string.collect do |word|
+    reversed_sentence << word.reverse
+  end
+  return reversed_sentence.join(" ")
+end
+
+
+#def reverse_each_word(sentence)
+ # new_array = sentence.split(" ")
+  #new_array.collect do |word|
+   # word.reverse
+ # end 
+  #new_array.join(" ")
+# end 
+
+# def reverse_each_word(sentence)
+  # new_array = sentence.split(" ")
+  # reverse_array = []
+  # new_array.each do |word|
+    #reverse_array << word.reverse
+  #end 
+  #return new_array.join(" ")
+#end
